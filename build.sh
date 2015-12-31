ecf955a94aaa03fd4512d11f4bc8fd8102bea90d
root <root@localhost.localdomain>
2015-12-31 14:08:06 +0100

    Add personal build script

---
+++ build.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Title: assembles, links and extracts shellcode from binary
+# File: build.sh
+# Author: Dennis 'dhn' Herrmann
+# SLAE-721
+
+build() {
+	local ARGV=${1}
+
+	echo '[+] Assembling ... '
+	nasm -f elf32 -o ${ARGV}.o ${ARGV}.nasm
+}
+
+link() {
+	echo '[+] Linking ...'
+	ld -o $1 $1.o
+}
+
+extract_shellcode() {
+	local ARGV=${1}
+
+	echo '[+] Extract Shellcode from binary ...'
+	echo ''
+	objdump -d ${ARGV}         \
+		| grep '[0-9a-f]:' \
+		| grep -v 'file'   \
+		| cut -f2 -d:      \
+		| cut -f1-6 -d' '  \
+		| tr -s ' '        \
+		| tr '\t' ' '      \
+		| sed 's/ $//g'    \
+		| sed 's/ /\\x/g'  \
+		| paste -d '' -s   \
+		| grep -oE '.{1,32}'
+	echo ''
+}
+
+build $1
+link $1
+extract_shellcode $1
+echo '[+] Done!'
